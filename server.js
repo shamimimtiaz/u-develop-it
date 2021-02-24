2443f868a0c032807e42ba8809f328fd15d6df26
Shamim Imtiaz <km_si@ymail.com>
2021-02-23 22:14:16 -0500

    add POST route

--- server.js
+++ server.js
@@ -45,7 +45,6 @@ db.all(sql, params, (err, rows) => {
   });
 });
 
-
 // Rap this with express.js for get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
     const sql = `SELECT * FROM candidates 
@@ -82,17 +81,31 @@ db.run(sql, params, function(err, result) {
   });
 });
 
-// //Create a candidate : create query for create operation
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-//               VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-// // ES5 function, not arrow function, to use this
-// db.run(sql, params, function(err, result) {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result, this.lastID);
-// });
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+//Create a candidate : create query for create operation
+const sql =  `INSERT INTO candidates (first_name, last_name, industry_connected) 
+                VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+  // ES5 function, not arrow function, to use this
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: body,
+      id: this.lastID
+    });
+  });
+});
 
 //Default response for any other requests(Not Found) Catch all
 app.use((req, res) => {
